12349da14327f5a1fb3d4e0a3f229e47c0da3a8f
“ouzerzou <mohamed.ouz2016@gmail.com>
2023-07-26 05:13:45 -0700

    fix betty

--- _builtin_2.c
+++ _builtin_2.c
@@ -2,7 +2,7 @@
 
 /**
  * change_dir - is the function for change dir
- * @directories: is the directory to go for it
+ * @target_dir: is the directory to go for it
  */
 
 void change_dir(char *target_dir)
@@ -37,7 +37,7 @@ void change_dir(char *target_dir)
 
 /**
  * cd_builtin - is the function that change the current working dir
- * @arrays: is the array where is stored the data
+ * @array: is the array where is stored the data
  */
 
 void cd_builtin(char **array)
@@ -47,12 +47,6 @@ void cd_builtin(char **array)
 	if (array[1] == NULL)
 	{
 		target_dir = _getenv("HOME");
-
-		/*if (target_dir == NULL)
-		{
-			write(STDERR_FILENO, "HOME not set\n", 13);
-			return;
-		}*/
 	}
 	else if (strcmp(array[1], "-") == 0)
 	{
@@ -74,19 +68,6 @@ void cd_builtin(char **array)
 		perror("getcwd");
 		return;
 	}
-
-	/*printf("Before change_dir:\n");
-	printf("PWD: %s\n", _getenv("PWD"));
-	printf("OLDPWD: %s\n", _getenv("OLDPWD"));*/
-
 	change_dir(target_dir);
-
-	/*printf("After change_dir:\n");
-	printf("PWD: %s\n", _getenv("PWD"));
-	printf("OLDPWD: %s\n", _getenv("OLDPWD"));*/
-
-	/*if (setenv("OLDPWD", oldpwd, 1) != 0)
-		perror("setenv");*/
-
 	free(oldpwd);
 }
